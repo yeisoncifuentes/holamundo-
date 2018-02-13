064a7217d229bd07581e31810a3a5eeea0605d11
Unknown <yeisoncifuentes2@gmail.com>
2018-02-13 09:28:55 -0500

    primera clase
    clase con suma de numeros resta, multiplicacion y division

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("clase de programacion II");
+		System.out.println("Hola mundo desde UDEC facataiva");
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio N 1");
+		System.out.println("");
+		System.out.println("");
+		int var1 = 20;
+		int var2 = 23;
+		System.out.println("la suma de la variable var1+var2="+(var1+var2));
+		
+				
+
+	}	
+
+}
